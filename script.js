2abcc61c01b394df6403708aaff756ff48684f71
Arif Hussain <arifh9821@gmail.com>
2023-12-22 15:57:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let ele=document.getElementById("counter");
+let inc=document.getElementById("incrementBtn");
+
+let cnt=0;
+function fn() {
+	alert(cnt)
+	cnt++;
+	ele.innerText=cnt;
+}
